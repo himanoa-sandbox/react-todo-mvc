8904ac4ba6c4235e45d8787eda939dde6bdff357
himanoa <matsunoappy@gmail.com>
2021-03-09 15:02:54 +0900

    task-list-loaderを実装

---
+++ src/data-loader/task-list-loader.ts
@@ -0,0 +1,27 @@
+import { Task, fromUnixtime } from '../view-model/task'
+import { FetchTaskListApi, fetchTaskListApi } from '../api/task-list-api'
+
+export type LoadTaskList = () => Promise<ReadonlyArray<Task>>;
+
+type Dependencies = {
+  fetchTaskListApi: FetchTaskListApi
+}
+// NOTE: APIからデータを取得してフロントエンドでもってるview-modelに変換して返す
+// NOTE: 依存性の注入 変換処理が複雑でテストしたいがAPIへのアクセスを内包していて、引数でAPIクライアントを受けとれるようにして、テストが書けるようにしている
+export const loadTaskList: (deps: Dependencies) => LoadTaskList =  (
+  deps
+) => async () => {
+  const taskResult = await deps.fetchTaskListApi()
+  return taskResult.tasks.map(t => {
+    return {
+      id: t.id,
+      name: t.name,
+      deliveryDate: fromUnixtime(t.deliveryDate)
+    }
+  })
+}
+
+// フロントエンドアプリケーションで使う実装を注入して依存性の解決
+export default loadTaskList({
+  fetchTaskListApi: fetchTaskListApi
+})
