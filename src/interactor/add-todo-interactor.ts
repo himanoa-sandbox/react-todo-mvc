6396f890e2aab9d0fc6d6bad6e583e663bf97d3f
himanoa <matsunoappy@gmail.com>
2021-03-09 16:33:05 +0900

    解説コメントを追加

--- src/interactor/add-todo-interactor.ts
+++ src/interactor/add-todo-interactor.ts
@@ -1,6 +1,9 @@
 import { Task, DeliveryDate } from '../view-model/task'
 
 // TODOを追加する。期限は現在の時間から一週間後にする
+// TODOを追加するというユーザーアクションから発生する物事を書く箇所
+// 肥大化しやすいので、肥大化しそうだなとおもったら細かくコードをsplitしていったほうがよい
+// このサンプルだと一週間後の日付を返す部分は切り出してもよい
 export const addTodo = (name: string, taskList: ReadonlyArray<Task>): Promise<ReadonlyArray<Task>> => {
   const lastTodoId = Math.max(...taskList.map(t => t.id))
 
